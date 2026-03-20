7037249732cddda31b4c1bb8cf344df1420b0560
jnikcbk <mmenrobloxfake@gmail.com>
2026-03-20 19:03:05 +0700

    Create node.js

---
+++ node.js
@@ -0,0 +1,12 @@
+{
+  "name": "tika-bot",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.x",
+    "dotenv": "^16.x"
+  }
+}
